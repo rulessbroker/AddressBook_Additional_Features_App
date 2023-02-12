7b7b3086bf58672ddfaeb2d0ad4ebf98f611a030
rulessbroker <rulessbroker>
2023-02-12 14:46:38 +0530

    Added: OAuth2 implementation in App

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.addressbookapp;
 
-import org.springframework.boot.SpringApplication;	
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.bridgelabz.addressbookapp.*")
-//@EnableOAuth2Sso
+@EnableOAuth2Sso
+@EnableAutoConfiguration
+@ComponentScan("com.bridgelabz.addressbookapp.*")
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
